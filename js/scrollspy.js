89ef85a72c80f93fc3cab462e738f150344f33c0
Jeff Hendy <jchendy@gmail.com>
2014-04-16 17:37:33 -0400

    minor tweaks to website code, no visible changes

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
     });
 
     var $body = $(document.body);
-    //var navHeight = $('.navbar').outerHeight(true) + 10;
 
     $body.scrollspy({
         target: '#nav-column',
